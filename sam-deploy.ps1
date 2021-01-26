c26a77313004bb4184e71062827862834e515600
Steven <steven.a.alcalde@gmail.com>
2021-01-26 08:23:57 -0800

    rework deployment scripts

--- sam-deploy.ps1
+++ sam-deploy.ps1
@@ -1,10 +1,23 @@
-sam deploy `
---template-file ./cloudformation/packaged.yaml `
+Param (
+    [Parameter(Mandatory=$true,
+    HelpMessage="Enter bucket name",
+    Position=0)]
+    [String]
+    $BucketName
+)
+
+$ExistingBucket = aws s3 ls | Where-Object {$_ -like "*$BucketName*"}
+if ($ExistingBucket -eq $null) {
+    Write-Output "Bucket $BucketName doesn't exists yet. Run the sam deploy ps script first."
+    exit
+}
+
+Write-Host 'sam deploy output:'
+sam deploy --template-file ./cloudformation/template.yaml `
 --stack-name "dev-portal" `
---s3-bucket dev-portal-artifax `
+--s3-bucket $BucketName `
 --capabilities CAPABILITY_NAMED_IAM `
 --parameter-overrides `
 DevPortalSiteS3BucketName="exp-dev-portal-static-assets" `
 ArtifactsS3BucketName="exp-dev-portal-artifacts" `
-CognitoDomainNameOrPrefix="exp" `
-StaticAssetRebuildMode=overwrite-content
+CognitoDomainNameOrPrefix="exp"
--- sam-package.ps1
+++ sam-package.ps1
@@ -1,3 +1,34 @@
-sam package --template-file ./cloudformation/template.yaml `
---output-template-file ./cloudformation/packaged.yaml `
---s3-bucket dev-portal-artifax
+Param (
+    [Parameter(Mandatory=$true,
+    HelpMessage="Enter bucket name",
+    Position=0)]
+    [String]
+    $BucketName,
+
+    [Parameter(Mandatory=$false,
+    HelpMessage="No profile specified. To use the default profile press Enter. Otherwise, enter a profile name",
+    Position=1)]
+    [AllowEmptyString()]
+    [String]
+    $AWSProfile
+)
+
+$ExistingBucket = aws s3 ls | Where-Object {$_ -like "*$BucketName*"}
+if ( $ExistingBucket ) {
+    Write-Output 'Bucket already exists'
+} else {
+    Write-Output 'Creating bucket...'
+    aws s3 mb "s3://$BucketName"
+}
+
+if ($AWSProfile -eq $null -or $AWSProfile -eq "") {
+    Write-Host Sam package output:
+    sam package --template-file ./cloudformation/template.yaml `
+    --output-template-file ./cloudformation/packaged.yaml `
+    --s3-bucket $BucketName `
+} else {
+    sam package --template-file ./cloudformation/template.yaml `
+    --output-template-file ./cloudformation/packaged.yaml `
+    --s3-bucket $BucketName `
+    --profile $AWSProfile
+}
